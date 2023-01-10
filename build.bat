f777a85919dea0be80a54c234c522438c0e21416
Majjcom <majjcom@126.com>
2023-01-10 18:03:19 +0800

    update build script

--- build.bat
+++ build.bat
@@ -1,2 +1,11 @@
 @echo off
 pyinstaller -F main.py -i NONE -n autoRunner --distpath ./build/dist --workpath ./build/build
+set auto_ver=%1
+if "%auto_ver%"=="" (
+    goto end
+) else (
+    goto frename
+)
+:frename
+REN .\build\dist\autoRunner.exe autoRunner-%auto_ver%-Win-x86_64.exe
+:end
