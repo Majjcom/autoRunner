e7e396b517fc7c6be8e78a3695bc71ac0241086a
Majjcom <majjcom@126.com>
2024-04-20 17:27:39 +0800

    new action

--- actions.py
+++ actions.py
@@ -10,14 +10,15 @@ import pyautogui
 
 
 _actions_available = {
-    'move_mouse_to',
-    'sleep',
-    'mouse_click',
-    'get_image_pos',
-    'screen_shot',
-    'remove_file',
-    'text_input',
-    'screen_shot_mem'
+    "move_mouse_to",
+    "sleep",
+    "mouse_click",
+    "get_image_pos",
+    "screen_shot",
+    "remove_file",
+    "text_input",
+    "text_input_file",
+    "screen_shot_mem",
 }
 
 _actions_map = dict()
@@ -27,42 +28,39 @@ class Action(Base):
     def __init__(self, config_: dict, root_):
         self._root = root_
         self._parse_config(config_)
-    
+
     def _parse_config(self, config_: dict) -> None:
         pass
-    
+
     def call(self) -> bool:
         return True
 
 
 def load_action(action_: dict, root) -> Action:
-    key_words = [
-        'type',
-        'action'
-    ]
+    key_words = ["type", "action"]
     check = utils.check_config_arguments(key_words, action_)
     if not check:
-        raise ParseErrorException('Loading action error, argument missing.')
-    if action_['type'] != 'action':
-        raise ParseErrorException('Loading action error, type error: {}'.format(action_['type']))
-    if action_['action'] not in _actions_available:
-        raise ParseErrorException('Action not support: {}'.format(action_['action']))
-    actobj = _actions_map[action_['action']]
+        raise ParseErrorException("Loading action error, argument missing.")
+    if action_["type"] != "action":
+        raise ParseErrorException(
+            "Loading action error, type error: {}".format(action_["type"])
+        )
+    if action_["action"] not in _actions_available:
+        raise ParseErrorException("Action not support: {}".format(action_["action"]))
+    actobj = _actions_map[action_["action"]]
     return actobj(action_, root)
 
 
 class Action_move_mouse_to(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'var'
-        ]
+        key_words = ["type", "action", "var"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_move_mouse_to error, missing argument: var')
-        self._var = config_['var']
-    
+            raise ParseErrorException(
+                "Loading Action_move_mouse_to error, missing argument: var"
+            )
+        self._var = config_["var"]
+
     def call(self) -> bool:
         target = self._root.get_var(self._var)
         pyautogui.moveTo(target)
@@ -71,16 +69,14 @@ class Action_move_mouse_to(Action):
 
 class Action_sleep(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'sleep_time'
-        ]
+        key_words = ["type", "action", "sleep_time"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_sleep error, argument missing: sleep_time')
-        self._sleep_time = config_['sleep_time']
-    
+            raise ParseErrorException(
+                "Loading Action_sleep error, argument missing: sleep_time"
+            )
+        self._sleep_time = config_["sleep_time"]
+
     def call(self) -> bool:
         time.sleep(self._sleep_time)
         return True
@@ -88,16 +84,14 @@ class Action_sleep(Action):
 
 class Action_mouse_click(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'push_time'
-        ]
+        key_words = ["type", "action", "push_time"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_mouse_click error, missing argument: push_time')
-        self._push_time = config_['push_time']
-    
+            raise ParseErrorException(
+                "Loading Action_mouse_click error, missing argument: push_time"
+            )
+        self._push_time = config_["push_time"]
+
     def call(self) -> bool:
         pyautogui.mouseDown()
         time.sleep(self._push_time)
@@ -107,32 +101,27 @@ class Action_mouse_click(Action):
 
 class Action_get_image_pos(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'var',
-            'base',
-            'image',
-            'precision'
-        ]
+        key_words = ["type", "action", "var", "base", "image", "precision"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_get_image_pos error, missing arguments.')
-        self._var = config_['var']
-        self._base = config_['base']
-        self._image = config_['image']
-        self._precision = config_['precision']
-    
+            raise ParseErrorException(
+                "Loading Action_get_image_pos error, missing arguments."
+            )
+        self._var = config_["var"]
+        self._base = config_["base"]
+        self._image = config_["image"]
+        self._precision = config_["precision"]
+
     def call(self) -> bool:
         # im_src
-        if self._base[0] == '$':
+        if self._base[0] == "$":
             data = self._root.get_var(self._base)
             im_src = utils.aircv_read_from_array(data)
         else:
             im_src = ac.imread(self._base)
-        
+
         # im_tgt
-        if self._image[0] == '$':
+        if self._image[0] == "$":
             data = self._root.get_var(self._image)
             im_tgt = utils.aircv_read_from_array(data)
         else:
@@ -140,24 +129,22 @@ class Action_get_image_pos(Action):
         get = ac.find_template(im_src, im_tgt)
         if get is None:
             return False
-        if get['confidence'] < self._precision:
+        if get["confidence"] < self._precision:
             return False
-        self._root.set_var(self._var, get['result'])
+        self._root.set_var(self._var, get["result"])
         return True
 
 
 class Action_screen_shot(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'save'
-        ]
+        key_words = ["type", "action", "save"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_screen_shot error, missing argument: save')
-        self._save = config_['save']
-    
+            raise ParseErrorException(
+                "Loading Action_screen_shot error, missing argument: save"
+            )
+        self._save = config_["save"]
+
     def call(self) -> bool:
         img = ImageGrab.grab()
         img.save(self._save)
@@ -166,18 +153,15 @@ class Action_screen_shot(Action):
 
 class Action_remove_file(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'ignore_error',
-            'path'
-        ]
+        key_words = ["type", "action", "ignore_error", "path"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException("Loading Action_remove_file error, missing arguments.")
-        self._ignore_error = config_['ignore_error']
-        self._path = config_['path']
-    
+            raise ParseErrorException(
+                "Loading Action_remove_file error, missing arguments."
+            )
+        self._ignore_error = config_["ignore_error"]
+        self._path = config_["path"]
+
     def call(self) -> bool:
         exists = os.path.exists(self._path)
         if not exists:
@@ -195,21 +179,17 @@ class Action_remove_file(Action):
 class Action_text_input(Action):
     def __init__(self, config_: dict, root_):
         super().__init__(config_, root_)
-        self._key_to_change = {
-            ' ': 'space'
-        }
-    
+        self._key_to_change = {" ": "space"}
+
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'text'
-        ]
+        key_words = ["type", "action", "text"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException("Loading Action_text_input error, missing argument: text")
-        self._text = config_['text']
-    
+            raise ParseErrorException(
+                "Loading Action_text_input error, missing argument: text"
+            )
+        self._text = config_["text"]
+
     def _press_key(self, key: str):
         if len(key) == 0:
             return
@@ -219,15 +199,55 @@ class Action_text_input(Action):
         if key.isupper():
             upper = True
         if upper:
-            pyautogui.keyDown('shiftleft')
+            pyautogui.keyDown("shiftleft")
         k = key
         if k in self._key_to_change:
             k = self._key_to_change[k]
         pyautogui.keyDown(k)
         pyautogui.keyUp(k)
         if upper:
-            pyautogui.keyUp('shiftleft')
-    
+            pyautogui.keyUp("shiftleft")
+
+    def call(self) -> bool:
+        for i in self._text:
+            self._press_key(i)
+        return True
+
+
+class Action_text_input_file(Action):
+    def __init__(self, config_: dict, root_):
+        super().__init__(config_, root_)
+        self._key_to_change = {" ": "space"}
+
+    def _parse_config(self, config_: dict) -> None:
+        key_words = ["type", "action", "file"]
+        check = utils.check_config_arguments(key_words, config_)
+        if not check:
+            raise ParseErrorException(
+                "Loading Action_text_input error, missing argument: text"
+            )
+        path = config_["file"]
+        with open(path, "r", encoding="utf_8") as f:
+            self._text = f.read()
+
+    def _press_key(self, key: str):
+        if len(key) == 0:
+            return
+        if len(key) > 1:
+            key = key[0]
+        upper = False
+        if key.isupper():
+            upper = True
+        if upper:
+            pyautogui.keyDown("shiftleft")
+        k = key
+        if k in self._key_to_change:
+            k = self._key_to_change[k]
+        pyautogui.keyDown(k)
+        pyautogui.keyUp(k)
+        if upper:
+            pyautogui.keyUp("shiftleft")
+
     def call(self) -> bool:
         for i in self._text:
             self._press_key(i)
@@ -236,16 +256,14 @@ class Action_text_input(Action):
 
 class Action_screen_shot_mem(Action):
     def _parse_config(self, config_: dict) -> None:
-        key_words = [
-            'type',
-            'action',
-            'var'
-        ]
+        key_words = ["type", "action", "var"]
         check = utils.check_config_arguments(key_words, config_)
         if not check:
-            raise ParseErrorException('Loading Action_screen_shot_to_mem error, missing argument.')
-        self._var = config_['var']
-    
+            raise ParseErrorException(
+                "Loading Action_screen_shot_to_mem error, missing argument."
+            )
+        self._var = config_["var"]
+
     def call(self) -> bool:
         image = ImageGrab.grab()
         arr = utils.pillow_save_as_array(image)
@@ -254,4 +272,4 @@ class Action_screen_shot_mem(Action):
 
 
 for i in _actions_available:
-    _actions_map[i] = eval('Action_' + i)
+    _actions_map[i] = eval("Action_" + i)
